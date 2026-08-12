e5af39996f1ea828d0c417331e8dab49082e0ec8
Adarsh puri <152283128+Adarshpuri1@users.noreply.github.com>
2026-08-13 01:51:47 +0530

    Time: 7 ms (39.61%), Space: 11.3 MB (13.87%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int low=0;
+        int high=0;
+        bool res= false;
+        unordered_map<char,int>s1mp;
+        unordered_map<char,int>s2mp;
+        for(int i=0 ; i< s1.size(); i++){
+            s1mp[s1[i]]++;
+        }
+        while(high<s2.size()){
+            s2mp[s2[high]]++;
+            while(high-low+1>s1.size()){
+                s2mp[s2[low]]--;
+                if(s2mp[s2[low]]==0){
+                    s2mp.erase(s2[low]);
+                }
+                low++;
+            }
+            if(high-low+1==s1.size()){
+                if(s2mp==s1mp){
+                    res=true;
+                }
+            }
+            high++;
+        }
+        return res;
+    }
+};
