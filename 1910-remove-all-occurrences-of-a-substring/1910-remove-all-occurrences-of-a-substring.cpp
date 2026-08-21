66dfba3a0609c6b06df802ff6b88739b0c9bf0d8
Adarsh puri <152283128+Adarshpuri1@users.noreply.github.com>
2026-08-21 22:11:34 +0530

    Time: 0 ms (100%), Space: 9.4 MB (72.17%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        int size=part.length();
-        while(s.length()>0 && s.find(part)<s.length()){
-            s.erase(s.find(part),size);
-        }
-        return s;
+        int len=part.length();
+      while(s.length()>0 && s.find(part)<s.length()){
+        int pos=s.find(part);
+        s.erase(pos,len);
+      }
+      return s;
     }
 };
