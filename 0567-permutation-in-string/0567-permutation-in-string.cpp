2b38041dbaa2c24f824dc078ace6d3616c3502fa
Adarsh puri <152283128+Adarshpuri1@users.noreply.github.com>
2026-08-21 22:31:16 +0530

    Time: 4 ms (52.31%), Space: 10.8 MB (21.02%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int low=0;
-        int high=0;
-        bool res= false;
         unordered_map<char,int>s1mp;
         unordered_map<char,int>s2mp;
-        for(int i=0 ; i< s1.size(); i++){
+        for(int i=0; i<s1.length(); i++){
             s1mp[s1[i]]++;
         }
-        while(high<s2.size()){
+        int low=0;
+        int high=0;
+        while(high<s2.length()){
             s2mp[s2[high]]++;
-            while(high-low+1>s1.size()){
+            while((high-low+1)>s1.size()){
                 s2mp[s2[low]]--;
                 if(s2mp[s2[low]]==0){
                     s2mp.erase(s2[low]);
                 }
                 low++;
             }
-            if(high-low+1==s1.size()){
-                if(s2mp==s1mp){
-                    res=true;
+            if((high-low+1)==s1.size()){
+                if(s1mp==s2mp){
+                    return true;
                 }
             }
             high++;
         }
-        return res;
+        return false;
     }
 };
